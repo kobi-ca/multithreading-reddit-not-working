7f920a9f4bf9183927de9ca0d45e6372a193973d
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2022-01-23 20:42:02 -0800

    introduced jthread
    with scope to make sure it all joins
    before the == check

--- modernized-main.cpp
+++ modernized-main.cpp
@@ -14,20 +14,15 @@ namespace {
 
 int main()
 {
-    std::array<std::thread, NUM_THREADS> threads;
     std::array<int, NUM_THREADS> working{0};
     int idx{};
-    for(auto& t : threads) {
-        // move op=
-        t = std::thread([&working](const int idx){ working[idx] = idx; }, idx++);
+    {
+        std::array<std::jthread, NUM_THREADS> threads;
+        for (auto &t: threads) {
+            // move op=
+            t = std::jthread([&working](const int idx) { working[idx] = idx; }, idx++);
+        }
     }
-
-    // cannot have jthread since we must make sure they all done before calling equal
-    for(auto& t : threads) {
-        // move op=
-        t.join();
-    }
-
     std::array<int, NUM_THREADS> verify{0};
     std::iota(verify.begin(), verify.end(), 0);
     std::clog << std::boolalpha << (verify == working) << '\n';
